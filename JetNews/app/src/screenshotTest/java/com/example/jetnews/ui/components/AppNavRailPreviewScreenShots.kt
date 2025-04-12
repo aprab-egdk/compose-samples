3a506310c1b87d81f6d8edee79248cc46c052cb8
aprab <aprab@eg.dk>
2025-04-12 15:52:34 +0530

    formatted file

--- JetNews/app/src/screenshotTest/java/com/example/jetnews/ui/components/AppNavRailPreviewScreenShots.kt
+++ JetNews/app/src/screenshotTest/java/com/example/jetnews/ui/components/AppNavRailPreviewScreenShots.kt
@@ -9,7 +9,8 @@ import com.example.jetnews.ui.theme.JetnewsTheme
 class AppNavRailPreviewScreenShots {
     @Preview("Drawer contents")
     @Preview("Drawer contents (dark)", uiMode = Configuration.UI_MODE_NIGHT_YES)
-    @Preview(name = "Drawer contents (landscape) (dark)",
+    @Preview(
+        name = "Drawer contents (landscape) (dark)",
         uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
     )
     @Composable
